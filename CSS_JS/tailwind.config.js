85ef02232fa4fe4fe32cbab41d21d8a0241b70aa
ferry237 <youbiferry@gmail.com>
2024-04-26 17:02:07 +0100

    premiere commit

---
+++ CSS_JS/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config= {
+    theme: {
+      extend: {
+        colors: {
+         search_col:'#F5F5F5',
+         nav2_bg:'#2E2E2E',
+         white:'#FFFFFF',
+         s1_bgcolor:'#211C24',
+         gris_clair:'#EDEDED',
+         gris_2:'#909090',
+         noir:'#000000',
+         gris_text:'#8B8B8B',
+         bg_gris:'#F7F7F7',
+         white2:'#F9F9F7',
+         gris:'#EAEAEA',
+         noirc:'#2C2C2C',
+         blanch_2:'#CFCFCF',
+         bl:'#00FFFF',
+         
+        },
+      }
+    }
+  }
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Project Title
+   conception d'un site de Ecommerce 
+## but du projet 
+ le but de ce projet est de realiser un site de Ecommerce dans le cardre de ma formation en devellopement web fullstack 
+
+
+## Authors
+
+Simo youbi ferry élian 
+
+
+## Tech Stack
+
+**Client:**
+
+ -HTML
+
+ -TailwindCSS
+
+**Design:** 
+
+  -Figma 
+           
+  -color hunt
+
+
